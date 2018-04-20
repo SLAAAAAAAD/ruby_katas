587235af93aa52f5af186ec7890fe56398c3797d
SLAAAAAAAD <alindman93@gmail.com>
2018-04-20 10:05:54 -0400

    initial commit

---
+++ katas/smallest_multiple.rb
@@ -0,0 +1,11 @@
+# from project euler, problem 5
+#
+#      "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#       What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
+#
+# Fill in the method to find the smallest number that is evenly divisible by all numbers up to and including an upper_bound
+# you may require 'prime' and use the prime class if you want
+
+def smallest_multiple(upper_bound)
+  # you awesome code here
+end
---
+++ specs/smallest_multiple_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative '../katas/smallest_multiple'
+# require_relative '../solutions/smallest_multiple'
+
+describe 'should return the smallest possible multiple' do
+  it 'should return the smallest possible multiple' do
+    expect(smallest_multiple(10)).to eq 2520
+    expect(smallest_multiple(11)).to eq 27720
+    expect(smallest_multiple(20)).to eq 232792560
+    expect(smallest_multiple(21)).to eq 232792560
+    expect(smallest_multiple(30)).to eq 2329089562800
+    expect(smallest_multiple(100)).to eq 69720375229712477164533808935312303556800
+  end
+end
